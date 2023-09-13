dcf6003a2bf792523c779c0695037a5bdfa2da74
Matthias Liffers <m@tthi.as>
2023-09-13 15:01:55 +0800

    Fiddlesticks

--- playground.R
+++ playground.R
@@ -9,6 +9,13 @@ getConsortiumDOIStats <- function(consortium) {
   content(response, "parsed")$meta
 }
 
+getConsortiumDOIStatsByYear <- function(consortium, year) {
+  url <- "https://api.datacite.org/dois"
+  queryString <- list("consortium-id" = consortium, "page[size]" = 1, published = year)
+  response <- VERB("GET", url, query = queryString, content_type("application/octet-stream"), accept("application/vnd.api+json"))
+  list(year = content(response, "parsed")$meta)
+}
+
 getClientData <- function(provider) {
   url <- "https://api.datacite.org/clients"
   queryString <- list("provider-id" = provider, "page[size]" = 1000)
@@ -39,13 +46,39 @@ dayCreated <- allClientData %>% map_depth(2, ~ .$attributes$created) %>% unlist 
 
 clientTable <- data.frame(clientId, clientTypes, dateCreated, yearCreated, monthCreated, dayCreated)
 
-doiStats <- getConsortiumDOIStats("ardc")
+doiStats <- getConsortiumDOIStats("ardc") # Get aggregate DOI stats for the consortium
 
-yearlyCreatedStats <- data.frame("year" = doiStats %>% .$created %>% map(~ .$id) %>% unlist, "count" = doiStats %>% .$created %>% map(~ .$count) %>% unlist)
-g <- ggplot(yearlyCreatedStats, aes(year, count))
-g + geom_col(fill = "blue") + labs(title = "DOIs created per year", colour = blues9) + xlab("Year") + ylab("DOIs created")
+# Chart DOIs minted by year
 
-yearlyPublishedStats <- data.frame("year" = doiStats %>% .$published %>% map(~ .$id) %>% unlist, "count" = doiStats %>% .$published %>% map(~ .$count) %>% unlist)
-published <- ggplot(yearlyPublishedStats, aes(year, count))
-published + geom_col(fill = "#FF9999", colour="black") + labs(title = "DOIs published per year", colour = blues9) + xlab("Year") + ylab("DOIs published")
+statsByYear <- getConsortiumDOIStatsByYear(consortium = "ardc", year = 2020)
+resourceTypes <- data.frame("type" = statsByYear %>% .$resourceTypes %>% map(~ .$title) %>% unlist, "total" = statsByYear %>% .$resourceTypes %>% map(~ .$count) %>% unlist)
+yearlyPublishedStats <- data.frame("year" = doiStats %>% .$published %>% map(~ .$id) %>% unlist, "total" = doiStats %>% .$published %>% map(~ .$count) %>% unlist)
+
+published <- ggplot(yearlyPublishedStats, aes(year, total))
+published +
+  geom_col(fill = "#00B0D5", colour="black") +
+  labs(title = "DOIs published per year") +
+  xlab("Year") + ylab("DOIs published") +
+  scale_y_continuous(
+    labels = scales::comma_format(big.mark = ',',
+                                  decimal.mark = '.'))
+
+# Collect categories per year
+
+yearlyResourceTypeStats <- yearlyPublishedStats %>% .$year %>% map(~ getConsortiumDOIStatsByYear(consortium = "ardc", year = .))
+refinedYearlyResourceTypeStats <- yearlyResourceTypeStats %>% map(~ .$resourceTypes)
+
+# Chart top ten minters of all time
+
+topTenMinters <- data.frame("repo" = doiStats %>% .$clients %>% map(~ .$title) %>% unlist, "count" = doiStats %>% .$clients %>% map(~ .$count) %>% unlist)
+minters <- ggplot(topTenMinters, aes(reorder(repo, count, decreasing = TRUE), count))
+minters +
+  geom_col(fill = "#00B0D5", colour="black") +
+  labs(title = "Top ten minters") +
+  xlab("Client") + ylab("DOIs published") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_y_continuous(
+    labels = scales::comma_format(big.mark = ',',
+                                  decimal.mark = '.'))
+
 
